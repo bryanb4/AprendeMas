95c98052f8f75816587cda517e144ee6e089f9e8
Aphediox <jorge.torres4847@alumnos.udg.mx>
2026-05-22 17:18:15 -0600

    ignorando archivos

---
+++ schemas/question_schema.js
@@ -0,0 +1,24 @@
+const questionSchema = {
+  type: "object",
+  properties: {
+    materia_solicitada: { type: "string" },
+    tema_solicitado: { type: "string" },
+    pregunta: { type: "string" },
+    opciones: {
+      type: "array",
+      items: { type: "string" }
+    },
+    respuesta_correcta: { type: "string" },
+    explicacion: { type: "string" }
+  },
+  required: [
+    "materia_solicitada",
+    "tema_solicitado",
+    "pregunta",
+    "opciones",
+    "respuesta_correcta",
+    "explicacion"
+  ],
+  additionalProperties: false
+};
+module.exports = questionSchema;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./rutas/authRoutes');
+const iaRoutes = require('./rutas/iaRoutes');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/results', authRoutes);
 app.use('/api/profile', authRoutes);
+app.use('/api/ia', iaRoutes);
 
 const PORT = process.env.PORT || 5000;
 
