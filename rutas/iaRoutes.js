77de98c71451a09b6dcef461af8ce719e059fa06
Aphediox <jorge.torres4847@alumnos.udg.mx>
2026-05-25 13:51:32 -0600

    Obtener primeras 3 preguntas con estado pendiente

--- rutas/iaRoutes.js
+++ rutas/iaRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getResponse, saveQuestion} = require('../controllers/iaControllers');
+const { getResponse, saveQuestion, primeras3Preguntas} = require('../controllers/iaControllers');
 
 router.post('/generate', getResponse);
 router.post('/guardarPregunta', saveQuestion);
+
+router.get('/primerasPreguntas', primeras3Preguntas);
 
 module.exports = router;
